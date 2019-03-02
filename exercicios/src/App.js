eadf147873a38455ed6bdc1a776d9f5c2936413a
Patrick Vieira Feitosa <patrick.vieira@enext.local>
2019-03-02 13:57:39 -0300

    Criado primeiro componente

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+/* Components */
+import Simples from './Components/Simples.js';
+
 export default class App extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.f20}>App!</Text>
+                <Simples textInside="Primeiro componente"/>
             </View>
         );
     };
