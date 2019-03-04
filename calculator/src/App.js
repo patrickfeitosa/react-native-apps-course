2836b4c067395d78554db128a0217e13cac58002
Patrick Vieira Feitosa <patrick.vieira@enext.local>
2019-03-03 22:39:52 -0300

    Finalizado a implementação da Calculadora

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -18,17 +18,22 @@ const styles = StyleSheet.create({
 export default class App extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
+        this.initialState = {
             displayValue: '0',
-        };
+            clearDisplay: false,
+            operation: null,
+            values: [0, 0],
+            current: 0
+        }
+
+        this.state = { ...this.initialState };
 
         this._createArrayButtons();
     }
 
     _createArrayButtons() {
         this.buttons = [
-            { label: 'AC', key: `${Math.random()}`, handleClick: () => this.clearMemory, styles: ['buttonTriple'] },
+            { label: 'AC', key: `${Math.random()}`, handleClick: () => this.clearMemory(), styles: ['buttonTriple'] },
             { label: '/', key: `${Math.random()}`, handleClick: (prop) => this.setOperation(prop), styles: ['operationButton'] },
             { label: '7', key: `${Math.random()}`, handleClick: (prop) => this.addDigit(prop) },
             { label: '8', key: `${Math.random()}`, handleClick: (prop) => this.addDigit(prop) },
@@ -48,25 +53,52 @@ export default class App extends Component {
         ];
     }
 
-
     addDigit(number) {
-        if (this.state.displayValue === '0') {
-            if (number === '.') {
-                this.setState({ displayValue: `0${number}` });
-            } else {
-                this.setState({ displayValue: `${number}` });
-            }
-        } else if (this.state.displayValue.indexOf('.') > 0 && number !== '.') {
-            this.setState({ displayValue: `${this.state.displayValue}${number}` });
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
+
+        if (number === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
+            return;
+        }
+
+        const currentValue = clearDisplay ? '' : this.state.displayValue;
+        const displayValue = currentValue + number;
+
+        this.setState({ displayValue, clearDisplay: false });
+
+        if (number !== '.') {
+            const newValue = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[this.state.current] = newValue;
+            this.setState({ values });
         }
     };
 
     clearMemory() {
-        this.setState({ displayValue: '0' });
+        this.setState({ ...this.initialState });
     };
 
     setOperation(operation) {
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1 });
+        } else {
+            const equals = operation === '=';
+            const values = [...this.state.values];
+            this.setState({ displayValue: `${values[0]}${values[1]}` })
+            try {
+                values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`);
+            } catch (err) {
+                values[0] = this.state.values[0];
+            }
 
+            values[1] = 0;
+            this.setState({
+                displayValue: `${values[0]}`,
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                values,
+            });
+        };
+        this.setState({ clearDisplay: true, });
     };
 
     render() {
